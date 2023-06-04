6ebcee072a8503b5f658d2f96a6cb36a6151d016
mohammed3599 <kataloni1999@gmail.com>
2023-06-04 09:47:01 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace git_practise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
